41d77e4a1d9bc42a59a5dd3cbeff16485a0bc872
KevinOFL <kevinsilvaofl@gmail.com>
2025-07-26 17:03:13 -0300

    Arquitetura completa da infraestrutura do projeto

--- terraform_arq/main.tf
+++ terraform_arq/main.tf
@@ -30,6 +30,8 @@ resource "aws_s3_bucket" "bucket_do_projeto" {
   
 }
 
+
+
 resource "aws_s3_bucket_versioning" "versionamento_do_meu_bucket" {
   # Vincula esta configuração ao bucket criado acima, usando o ID do bucket.
   bucket = aws_s3_bucket.bucket_do_projeto.id
@@ -60,4 +62,58 @@ resource "aws_s3_object" "refined" {
 # Trazendo o IAM LabRole
 data "aws_iam_role" "minha_role_existente" {
   name = "LabRole"
+}
+
+# Zipando o código fonte da função lambda
+data "archive_file" "lambda_zip" {
+  type = "zip"
+  source_file = "${path.module}/src/trigger_glue.py"
+  output_path = "${path.module}/dist/trigger_glue.zip"
+}
+
+# Criação da função lambda para fazer a ponte do arquivo para o Glue
+resource "aws_lambda_function" "scrapping_b3_lambda" {
+  # Nome da função
+  function_name = "GlueTriggerlambda"
+  role = data.aws_iam_role.minha_role_existente.arn # Role para poder ter o poder de acessar o S3
+
+  filename = data.archive_file.lambda_zip.output_path # Local onde se encontra o zip do código
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  # Configurações de uso do código
+  handler = "trigger_glue.lambda_handler"
+  runtime = "python3.11"
+  timeout = 30
+
+  # Criação da variavel de ambiente no lambda
+  environment {
+    variables = {
+      GLUE_JOB_NAME = "ETL_glue_pregao_B3"
+    }
+  }
+  tags = {
+   ManageBy = "Terraform"
+  }
+}
+
+# Criando a permissão para o S3 poder invocar a função lambda
+resource "aws_lambda_permission" "allow_s3_to_invoke" {
+  statement_id = "AllowS3ToInvokeLambda" # Nome da regra de politica
+  action = "lambda:InvokeFunction" # Especificando qual a ação será acionada
+  function_name = aws_lambda_function.scrapping_b3_lambda.function_name # Função que será acionada
+  principal = "s3.amazonaws.com" # Serviço que pode realizar a invocação
+  source_arn = aws_s3_bucket.bucket_do_projeto.arn # Especificando qual S3 pode exclusivamente acionar
+}
+
+# Criando o gatilho de invocação da lambda
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.bucket_do_projeto.id # Bucket que acionara
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.scrapping_b3_lambda.arn # Função lambda
+    events = ["s3:ObjectCreated:Put", "s3:ObjectCreated:CompleteMultipartUpload"] # Qual o tipo de evento gatilho. No caso o de criação de um novo arquivo
+    filter_prefix = "raw/" # Gatilho só para a pasta raw/
+  }
+
+  depends_on = [ aws_lambda_permission.allow_s3_to_invoke ] # Dependencia da permissão acima
 }
