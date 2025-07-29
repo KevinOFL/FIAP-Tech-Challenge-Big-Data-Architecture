0008b6f8706a1063339943a9e604a1d4f28ae7d7
KevinOFL <kevinsilvaofl@gmail.com>
2025-07-29 16:41:22 -0300

    att scraping.py

--- terraform_arq/src/scraping_b3.py
+++ terraform_arq/src/scraping_b3.py
@@ -23,7 +23,6 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(
 aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
 aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
 aws_session_token = os.getenv("AWS_SESSION_TOKEN")  
-nome_bucket = os.getenv("NOME_BUCKET_RAW")
 bucket_name = os.getenv("BUCKET_NAME")
 
 # Criando agente boto3
